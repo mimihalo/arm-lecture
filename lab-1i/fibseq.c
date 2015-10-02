17b29fd3c50fee089aee1f45794e1b509efea0c5
mimihalo <mimihalo@miko.tw>
2015-10-02 23:39:57 +0800

    Add function of reading PMU CCNT
    Add the function of reading PMU CCNT.
    Can use parameter to excute the program.
    
    	modified:   fibseq.c

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
+#include <stdint.h>
 
 extern int fibonacci(int x);
+
+unsigned ccnt_read ()
+{
+  volatile unsigned cc;
+  asm volatile ("mrc p15, 0, %0, c9, c13, 0" : "=r" (cc));
+  return cc;
+
+}
 
 int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
+  unsigned long cycstart, cycend, cyc=0;
 
-  scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("fib(%d) = %d\n", number, result);
+  if(argc==0)
+    scanf("%d",&number);
+  else
+  {
+    number=atoi(argv[1]);
+  }
+  cycstart=ccnt_read;
+  result = fibonacci(number);
+  cycend=ccnt_read;
+  cyc=cycend-cycstart;
+  printf("fib(%d) = %d , cycle count = %lu\n", number, result,cyc);
 }
 
